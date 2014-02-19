2ba38b78f09833251dc5c75ef24d1b44a8a79e35
hdznrrd <hadez@nrrd.de>
2014-02-19 22:28:21 +0100

    now accepts raw output from convert histogram extraction

--- ip/histbin_to_magickdraw.pl
+++ ip/histbin_to_magickdraw.pl
@@ -11,19 +11,21 @@ my @data = ();
 my $sum = 0;
 while(<>)
 {
-	chomp; chomp;
-	my ($count,$color) = split /:/;
-	push @data, [$count,$color];
-	$sum += $count;
+	#27839: (255,255,255) #FFFFFF white
+	if(/(\d+):[^#]+(#.{6})/)
+	{
+		push @data, [$1,$2];
+		$sum += $1;
+	}
 }
 
 my $ppc = $WIDTH/$sum;
 my $pos = 0;
 foreach(@data)
 {
-	my $w = int($ppc*$_->[0]);
+	my $w = 1+int($ppc*$_->[0]);
 	my $c = $_->[1];
-	printf " -fill $c -draw \'rectangle %d,%d %d,%d\' ", $pos, $HEIGHT, $pos+$w, $HEIGHT;
+	printf " -draw \'fill $c rectangle %d,%d %d,%d' ", $pos, 0, $pos+$w, $HEIGHT;
 	$pos += $w;
-	return if($pos >= $WIDTH);
+	last if($pos >= $WIDTH);
 }
